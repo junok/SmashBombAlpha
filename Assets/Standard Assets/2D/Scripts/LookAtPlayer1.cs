dce651849088b605bf45a6d2ea1e17c078bc3022
junok <u0830932@utah.edu>
2016-11-17 21:23:47 -0700

    fixed collider going through walls

---
+++ Assets/Standard Assets/2D/Scripts/LookAtPlayer1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class LookAtPlayer1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.DOLookAt(GameObject.Find("Player").transform.position, 0, AxisConstraint.None, Vector3.up);
+	}
+}
